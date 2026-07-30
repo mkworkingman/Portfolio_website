bc87e87e356858518ce4682cbcabe8901542c06f
mkworkingman <mkworkingman@gmail.com>
2026-07-30 19:08:30 +0200

    Remove useless files

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,8 +1,6 @@
 import { initClickHandler } from './clickHandler'
 import { initLanguage } from './language'
-// import { initToggleNavBar } from './navbar'
 
 // TODO: Loading spinner before scripts are loaded?
 initLanguage()
-// initToggleNavBar()
 initClickHandler()
