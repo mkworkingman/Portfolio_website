c43f83dfa6c1bb0a482463cb1601604e532bc122
mkworkingman <mkworkingman@gmail.com>
2026-08-18 20:52:15 +0200

    Font settings

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,6 +1,7 @@
+import '@fontsource/montserrat'
 import { initClickHandler } from './clickHandler'
 import { initLanguage } from './language'
 
-// TODO: Loading spinner before scripts are loaded?
+// TODO: Loading spinner or before font and scripts are loaded? Fade in after loading is done?
 initLanguage()
 initClickHandler()
